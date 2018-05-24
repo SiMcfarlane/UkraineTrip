3e6d1d46283eca6e066f2d60cc87bc3990c4ee30
Silas McFarlane <silasmcfarlane14@gmail.com>
2018-05-23 20:25:50 -0700

    Added Menu functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const ul = document.querySelector('.main-ul');
+const hamb = document.querySelector('.menu');
+const close = document.querySelector('.close');
+const header = document.querySelector('.main-header');
+
+hamb.addEventListener('click', openNav);
+close.addEventListener('click', closeNav);
+
+function openNav() {
+    ul.style.top = "30px";
+    header.style.paddingTop = "250px";
+    hamb.style.display = "none";
+    close.style.display = "block";
+}
+function closeNav() {
+    ul.style.top = "-250px";
+    header.style.paddingTop = "0";
+    hamb.style.display = "block";
+    close.style.display = "none";
+    
+}
